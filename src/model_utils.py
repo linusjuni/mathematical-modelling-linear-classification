042aa963e0ae8853156385c48d424b3f92a9b3e7
Linus Juni <linus.juni@eksponent.com>
2025-04-16 23:11:22 +0200

    training model update print

--- src/model_utils.py
+++ src/model_utils.py
@@ -4,6 +4,7 @@ from sklearn.metrics import accuracy_score
 def train_models(X_train, y_train, lambda_values):
     models = {}
     for lambda_val in lambda_values:
+        print(f"Training model with lambda: {lambda_val}...")
         model = LogisticRegression(C=1/lambda_val, penalty='l2', solver='lbfgs', max_iter=1000)
         model.fit(X_train, y_train)
         models[lambda_val] = model
