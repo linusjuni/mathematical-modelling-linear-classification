24fd6426e2a5317abf6070589eaf3f2fa2ac8eed
Linus Juni <linus@juni.dk>
2025-05-11 13:43:38 +0200

    fixing some other models

--- src/gradient_boost.py
+++ src/gradient_boost.py
@@ -1,5 +1,5 @@
 import os
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import HistGradientBoostingClassifier # Use this for speed
 from sklearn.metrics import roc_auc_score, accuracy_score
 
 from data_utils import load_data, load_data_with_sobel_kernel, load_data_with_histograms_of_orientation
@@ -15,17 +15,16 @@ X_train, y_train = load_data(train_path)
 print("Loading test data...")
 X_test, y_test = load_data(test_path)
 
-gb_clf = GradientBoostingClassifier(n_estimators=50, max_depth=3, learning_rate=0.1)
+gb_clf = HistGradientBoostingClassifier(max_iter=50, max_depth=3, learning_rate=0.1, verbose=1) 
 print("Training Gradient Boosting classifier...")
 gb_clf.fit(X_train, y_train)
 
-print("Making predictions on the test set...")
+print("Evaluating Gradient Boosting classifier...")
 y_pred = gb_clf.predict(X_test)
-y_pred_proba = gb_clf.predict_proba(X_test)[:, 1]
+y_pred_proba = gb_clf.predict_proba(X_test)[:, 1] # Probabilities for the positive class
 
-auc = roc_auc_score(y_test, y_pred_proba)
 accuracy = accuracy_score(y_test, y_pred)
+auc = roc_auc_score(y_test, y_pred_proba)
 
-print(f"Gradient Boosting Classifier Performance:")
-print(f"AUC: {auc:.4f}")
 print(f"Accuracy: {accuracy:.4f}")
+print(f"AUC: {auc:.4f}")
